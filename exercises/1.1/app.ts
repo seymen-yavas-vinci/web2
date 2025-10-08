73feaa4ab60c8e12a4ba9fa7c7f197ca023372d6
Yavas Seymen <ohseyy@MacBook-Air-de-Yavas.local>
2025-10-08 11:08:54 +0200

    ex1.1: GET /films (READ ALL) dans exercises/1.1 corr prof

--- exercises/1.1/app.ts
+++ exercises/1.1/app.ts
@@ -1,16 +1,12 @@
 import express from "express";
 
-import usersRouter from "./routes/users";
-import pizzaRouter from "./routes/pizzas";
-import filmsRouter from "./routes/film";
+import pizzaRouter from "./routes/films";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/users", usersRouter);
-app.use("/pizzas", pizzaRouter);
-app.use("/films", filmsRouter);
+app.use("/films", pizzaRouter);
 
 export default app;
--- exercises/1.1/types.ts
+++ exercises/1.1/types.ts
@@ -1,14 +1,11 @@
-interface Pizza {
+interface Film {
   id: number;
   title: string;
-  content: string;
+  director: string;
+  duration: number;
+  budget?: number;
+  description?: string;
+  imageUrl?: string;
 }
 
-interface PizzaToUpdate {
-  title?: string;
-  content?: string;
-}
-
-type NewPizza = Omit<Pizza, "id">;
-
-export type { Pizza, NewPizza, PizzaToUpdate };
+export type { Film};
