bafba7cb14b73a2fce1381c0589a9e85edcaa0ea
Yavas Seymen <ohseyy@MacBook-Air-de-Yavas.local>
2025-10-20 12:08:59 +0200

    new:ex2.3

---
+++ tutorials/collections/src/components/App/index.tsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+import Header from '../Header';
+import Main from '../Main';
+import Footer from '../Footer';
+// import pizzaBackground from "./assets/images/pizza.jpg";
+
+
+
+function App() {
+  return (
+    <div className="page">
+      <Header></Header>
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+
+
+export default App;
---
+++ tutorials/collections/src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+ import "./Header.css"
+ 
+const Header = () => {
+  return (
+    <header>
+      <h1 className="animate__animated animate__bounce">We love Pizza</h1>
+    </header>
+  );
+};
+
+export default Header;
